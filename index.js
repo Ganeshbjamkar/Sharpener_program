ba08748d3c8e6f14be0cb2ab1b784ee829846447
Ganeshbjamkar <you@example.com>
2023-05-23 18:11:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// alert("Welcome to the JavaScript");
+
+// confirm("Are you sure you want to learn JavaScript")
+
+// prompt("Please enter your name","Seattle")
+
+// document.write(" <h1> Jamkar </h1>");
+
+// console.log("Welcome to the javascript");
+// console.log("Welcome to Web Development");
+// console.log("Hi");
+
+// var x = document.getElementById("main").innerHTML;
+// console.log(x);
+
+document.getElementById("main").innerHTML = "<h4>web application development</h4>";
+
+var x = document.getElementsByClassName("main")[0].innerHTML;
+alert(x);
